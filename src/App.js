cf84dba89082b95d70d96f290ecbcb23cf0b2500
jaivik1920 <pateljaivik2025@gmail.com>
2024-12-09 17:50:37 -0500

    comiit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 export const questions = [
   {
-    question: "What does 'CI/CD' stand for in DevOps??????",
+    question: "What does 'CI/CD' stand for in DevOpSSS?",
     options: [
       "Continuous Integration/Continuous Delivery",
       "Critical Infrastructure/Cloud Deployment",
